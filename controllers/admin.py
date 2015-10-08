165e9135ef19c2e61df76edf534aa865242c7a2d
Yotam <yotammanor@gmail.com>
2015-10-08 20:36:30 +0300

    added admin_current_state basic html and setup for data.

--- controllers/admin.py
+++ controllers/admin.py
@@ -1,6 +1,7 @@
 import os
 from flask import Blueprint, current_app, g, redirect, abort, jsonify, send_from_directory
 from models.event import Event
+from models.cart import Cart
 from datetime import datetime, timedelta
 
 TIME_UNIT = 'minutes'
@@ -28,12 +29,21 @@ def admin_base():
     file_path_directory = os.path.join(ctrl.static_folder, "admin")
     return send_from_directory(file_path_directory, 'admin_base.html')
 
+
 @ctrl.route('/admin/log/')
 def admin_log_view():
     file_path_directory = os.path.join(ctrl.static_folder, "admin")
     return send_from_directory(file_path_directory, 'admin_log.html')
 
 
-@ctrl.route("/admin/current-state/")
+@ctrl.route('/admin/state/')
+def admin_current_state_view():
+    file_path_directory = os.path.join(ctrl.static_folder, "admin")
+    return send_from_directory(file_path_directory, 'admin_current_state.html')
+
+
+@ctrl.route("/admin/state/data/")
 def current_state_of_carts():
-    return jsonify(title='this is the current state of cards')
+    carts = Cart.objects()
+    return jsonify(title='As of {}'.format(datetime.now()), msg=[cart.toMinimalJson() for cart in carts])
+
