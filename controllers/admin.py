cbc934a6997a2e8abdeca90e11b63c7091479969
Yotam <yotammanor@gmail.com>
2015-10-08 17:30:51 +0300

    return all events from last arbitrary time measurements.

--- controllers/admin.py
+++ controllers/admin.py
@@ -1,5 +1,11 @@
 from flask import Blueprint, current_app, g, redirect, abort, jsonify
 from models.event import Event
+from datetime import datetime, timedelta
+
+TIME_UNIT = 'seconds'
+TIME_MEASUREMENT = 4
+
+
 
 class AdminController(Blueprint):
     pass
@@ -12,5 +18,5 @@ ctrl = AdminController("admin", __name__, static_folder="../public")
 def activity_log():
     event = Event(name='bla')
     event.save()
-    events = Event.objects()
-    return jsonify(msg=[event.toMinimalJson() for event in events])
+    events = Event.objects(creation_time__gte=datetime.now() - timedelta(**{TIME_UNIT: TIME_MEASUREMENT}))
+    return jsonify(title='events from the last {} {}'.format(TIME_MEASUREMENT, TIME_UNIT), msg=[event.toMinimalJson() for event in events])
