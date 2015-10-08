2e2e0b8bf87347cd2d839e787d9f7581444010e0
Yotam <yotammanor@gmail.com>
2015-10-08 19:22:42 +0300

    pep8 changes.

--- models/user.py
+++ models/user.py
@@ -4,37 +4,39 @@ from mongoengine import *
 from lib.id_generator import randomIdGenerator
 from login import Login
 
+from models.cart import Cart
+
 
 class User(Document):
-  """
-  This model represents a User in the database.
+    """
+    This model represents a User in the database.
 
-  It contains all of the information we have on a specific user, along with
-  profile information to be used by communication and collection algorithms.
-  """
-  user_id = StringField(primary_key=True,
-                        default=randomIdGenerator("U"))
+    It contains all of the information we have on a specific user, along with
+    profile information to be used by communication and collection algorithms.
+    """
+    user_id = StringField(primary_key=True,
+                          default=randomIdGenerator("U"))
 
-  login = ReferenceField(Login)
+    login = ReferenceField(Login)
 
-  creation_time = DateTimeField(default=datetime.datetime.now)
+    creation_time = DateTimeField(default=datetime.datetime.now)
 
-  cart = ReferenceField(Cart)
+    cart = ReferenceField(Cart)
 
-  def toMinimalJson(self):
-    return {
-        "userId": str(self.user_id),
-        "username": str(self.login.username) if self.login else None,
-        "authenticated": (bool(self.login.authenticated)
-                          if self.login and self.login.authenticated else None),
-        "cartId": self.cart.cart_id if self.cart else None
-      }
+    def toMinimalJson(self):
+        return {
+            "userId": str(self.user_id),
+            "username": str(self.login.username) if self.login else None,
+            "authenticated": (bool(self.login.authenticated)
+                              if self.login and self.login.authenticated else None),
+            "cartId": self.cart.cart_id if self.cart else None
+        }
 
-  def toFullJson(self):
-    return self.toMinimalJson()
+    def toFullJson(self):
+        return self.toMinimalJson()
 
-  def __str__(self):
-    return "(%(user_id)s)" % self._data
+    def __str__(self):
+        return "(%(user_id)s)" % self._data
 
-  def __repr__(self):
-    return self.__str__()
+    def __repr__(self):
+        return self.__str__()
