5d59190ed66cff47875a1094266164e1faeba89a
Yotam <yotammanor@gmail.com>
2015-10-09 01:46:14 +0300

    refactored to work with admin authentication.

--- public/admin/scripts/admin_log.js
+++ public/admin/scripts/admin_log.js
@@ -1,17 +1,31 @@
 $(document).ready(function () {
     var tableBody = $('#log-table');
     $.ajax({
-        url: 'data/',
-        data: [],
+        url: '/auth/login/',
+        data: JSON.stringify({
+            username: 'admin',
+            password: 'Password1'
+        }),
+        type: 'POST',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
         success: function (data) {
-            var res = data.msg;
-            $('#log-title').html(data.title);
-            for (var i = 0; i < res.length; i++) {
-                console.log(res[i]);
-                console.log(tableBody);
-                tableBody.append("<tr><td>" + res[i]['createdAt'] + "</td> <td>" + res[i]['eventId'] + "</td> <td>" + res[i]['user'] + "</td> <td>" + res[i]['cart'] + "</td> <td>" + res[i]['name'] + "</td></tr>")
-            }
-        }
+            console.log(data);
+            $.ajax({
+                url: '/api/admin/log/data/?access_token=' + data['access_token'],
+                data: [],
+                success: function (data) {
+                    var res = data.msg;
+                    $('#log-title').html(data.title);
+                    for (var i = 0; i < res.length; i++) {
+                        console.log(res[i]);
+                        console.log(tableBody);
+                        tableBody.append("<tr><td>" + res[i]['createdAt'] + "</td> <td>" + res[i]['eventId'] + "</td> <td>" + res[i]['user'] + "</td> <td>" + res[i]['cart'] + "</td> <td>" + res[i]['name'] + "</td></tr>")
+                    }
+                }
 
-    })
+            })
+        }
+    });
+
 });
