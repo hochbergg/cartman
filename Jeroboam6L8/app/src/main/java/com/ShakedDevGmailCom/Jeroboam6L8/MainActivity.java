a775f804eb77b57a9407f6a7243de2426363367a
Agam Rafaeli <agam.rafaeli@gmail.com>
2015-10-08 22:25:16 +0300

    Working monitoring listener

--- Jeroboam6L8/app/src/main/java/com/ShakedDevGmailCom/Jeroboam6L8/MainActivity.java
+++ Jeroboam6L8/app/src/main/java/com/ShakedDevGmailCom/Jeroboam6L8/MainActivity.java
@@ -1,16 +1,30 @@
 package com.ShakedDevGmailCom.Jeroboam6L8;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private IntentFilter intentFilter;
+    private BroadcastReceiver myReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String BeaconName = intent.getStringExtra("Beacon Name");
+            addText(BeaconName);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter("com.hmkcode.android.USER_ACTION");
     }
 
     @Override
@@ -33,5 +47,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(myReceiver , intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(myReceiver);
+        super.onPause();
+    }
+
+    private void addText(String newText) {
+        TextView theView = (TextView) findViewById(R.id.textView1);
+        String oldText = theView.getText().toString();
+        theView.setText(oldText + "\n" + newText);
+//        theView.setText(newText);
     }
 }
